9275cd1e6e2577e57c7853558588c9c471255853
Elizaveta Guseva <liza.guseva@hey.com>
2023-04-03 17:03:51 +0200

    mystery

--- descobridor/discovery/read_raw_reviews.py
+++ descobridor/discovery/read_raw_reviews.py
@@ -13,8 +13,7 @@ from descobridor.discovery.constants import (
     TOO_MANY_PAGES, 
     REVIEWS_TOO_OLD_MONTHS,
     GMAPS_NEXT_PAGE_TOKEN,
-    PAGE_STATUS_EXPIRATION,
-    RAW_PAGE_EXPIRATION_S
+    PAGE_STATUS_EXPIRATION
     )
 from descobridor.the_logger import logger
 
@@ -96,7 +95,7 @@ def make_page_record(
     
 def store_page(record: Dict[str, Any]) -> None:
     with CosmosConnection("raw_reviews") as conn:
-        conn.collection.insert_one(record, ttl=RAW_PAGE_EXPIRATION_S)
+        conn.collection.insert_one(record)
     return record
      
      
